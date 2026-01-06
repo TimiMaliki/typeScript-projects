f0d64c765b9ca27e3c626433489ca34924c61b5b
TimiMaliki <malikitimi@gmail.com>
2026-01-06 13:55:53 +0100

    challenge

--- typescript-course/src/course1.ts
+++ typescript-course/src/course1.ts
@@ -76,3 +76,12 @@ randomValue = randomValue = true
 randomValue = randomValue = "random"
 
 // Challenge
+
+// Create a variable orderStatus of type 'processing' | 'shipped' | 'delivered' and assign it the value 'processing'. Then, try to assign it the values 'shipped' and 'delivered'.
+// Create a variable discount of type number | string and assign it the value 20. Then, try to assign it the value '20%'
+
+let orderStatus : "processing" | "shipped" | "delivered" = "processing"
+ orderStatus = "shipped"
+
+ let discount : number | string = 20
+ discount = "20%"
