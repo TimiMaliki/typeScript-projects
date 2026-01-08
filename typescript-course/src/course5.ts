26e041200995fea08b788d753d96a00e1db43b0b
TimiMaliki <malikitimi@gmail.com>
2026-01-08 13:37:54 +0100

    type interface

---
+++ typescript-course/src/course5.ts
@@ -0,0 +1,18 @@
+// Type InterFaces in TypeScript
+
+interface Book {
+  readonly isbn: number;
+  title: string;
+  author: string;
+  genre?: string;
+}
+
+const deepWork: Book = {
+  isbn: 9781455586691,
+  title: 'Deep Work',
+  author: 'Cal Newport',
+  genre: 'Self-help',
+};
+
+deepWork.title = 'New Title'; // allowed
+// deepWork.isbn = 654321; // not allowed
