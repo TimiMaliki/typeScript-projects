bd1377e335e78a4426c717e6c8668d4717b93506
TimiMaliki <malikitimi@gmail.com>
2026-01-12 15:02:17 +0100

    Enums

--- typescript-course/src/course7.ts
+++ typescript-course/src/course7.ts
@@ -100,3 +100,27 @@ function checkTheme(theme: Theme) {
   theme;
   // theme is of type never, because it can never have a value that is not 'light' or 'dark'.
 }
+
+enum Color {
+  Red,
+  Blue,
+  // Green,
+}
+
+function getColorName(color: Color) {
+  switch (color) {
+    case Color.Red:
+      return 'Red';
+    case Color.Blue:
+      return 'Blue';
+    default:
+      // at build time
+      let unexpectedColor: never = color;
+      // at runtime
+      throw new Error(`Unexpected color value: ${unexpectedColor}`);
+  }
+}
+
+console.log(getColorName(Color.Red)); // Red
+console.log(getColorName(Color.Blue)); // Blue
+// console.log(getColorName(Color.Green)); // Green
