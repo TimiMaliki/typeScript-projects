f2ef40f446c14df7c842c91420d86c89e93aa70d
TimiMaliki <malikitimi@gmail.com>
2026-01-31 22:12:16 +0100

    digital-store-mockup

--- digitalStoreMockup/digitalstore/src/App.tsx
+++ digitalStoreMockup/digitalstore/src/App.tsx
@@ -6,12 +6,8 @@ function App() {
  
   return (
     <>
-  <h1 className="text-3xl font-bold underline bg-black">
-    Hello world!
-  </h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+ 
+      
     </>
   )
 }
